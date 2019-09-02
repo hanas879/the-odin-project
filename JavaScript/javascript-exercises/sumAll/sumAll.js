74c683a65b8b560a85515b4e611f34601e58bb38
Henrik Enger Milo <hanas879@milo.life>
2019-09-02 15:28:07 +0200

    Work on project

--- JavaScript/javascript-exercises/sumAll/sumAll.js
+++ JavaScript/javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num, endNum) {
+    if(num < 0 || endNum < 0) {
+        return "ERROR"
+    }
+    
+    let sum = 0
+    for(let i = num; i < endNum + 1; i++) {
+        sum += i
+    }
+    return sum
 }
 
 module.exports = sumAll
