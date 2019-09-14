3b7b53764cc1d4fa09b8121020bd20aaf0321dbb
Henrik Enger Milo <hanas879@hotmail.com>
2019-09-14 18:45:44 +0200

    Finish palindromes exercise

--- JavaScript/javascript-exercises/palindromes/palindromes.js
+++ JavaScript/javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function(word) {
+    word = word.toLowerCase().replace(/[^A-Za-z]/g, "")
+    let reverseWord = word.toLowerCase().replace(/[^A-Za-z]/g, "").replace(" ", "").split("").reverse().join("")
 
+    return word == reverseWord
 }
 
 module.exports = palindromes
