9ff6dcb8af7612df5fd75725e1d7c93f9ca439ef
Henrik Enger Milo <hanas879@milo.life>
2019-09-27 15:27:06 +0200

    Work on project

--- Ruby/ruby_basic/orangeTree.rb
+++ Ruby/ruby_basic/orangeTree.rb
@@ -1,12 +1,20 @@
+is_playing = true
+tree = 0
+
 class OrangeTree
     def initialize
         @height = 0
         @age = 0
         @date = Time.new
         @orange_count = 0
+        @yearly_yield_multiplier = 12
         puts "You planted a tree on " + @date.day.to_s + "." + @date.month.to_s + "." + @date.year.to_s + "!"
     end
     
+    def date
+        puts "The current date is: " + @date.day.to_s + "." + @date.month.to_s + "." + @date.year.to_s + "!"
+    end
+
     def height
         if @height < 100
             puts "The tree is " + @height.to_s + "cm high!"
@@ -24,7 +32,10 @@ class OrangeTree
             @date += 31536000
             @age += 1
             @height += 30
-            height
+        end
+
+        if @age >= 3
+            @orange_count = @age * @yearly_yield_multiplier
         end
     end
 
@@ -32,20 +43,74 @@ class OrangeTree
         if @orange_count == 0
             puts "There is no oranges on the tree. Better luck next year"
         else
-            puts "You see a total of " + @orange_count.to_s + "oranges"
+            puts "You see a total of " + @orange_count.to_s + " oranges"
+        end
+    end
+
+    def age
+        puts "Your tree is " + @age.to_s + " years old!"
+    end
+
+    def randPhrase
+        rand_num = rand(1..4)
+        
+        if rand_num == 1
+            puts "You take a bite and it taste like heaven!"
+        elsif rand_num == 2
+            puts "The sweetness of the orange fills the body with joy"
+        elsif rand_num == 3
+            puts "Yummi!"
+        elsif rand_num == 4
+            puts "Munch.... SLURP"
+        end
+    end
+
+    def pickAnOrange
+        if @orange_count > 0
+            @orange_count -= 1
+            randPhrase
+        else
+            puts "There is no oranges to pick... Wait until next year and try again"
         end
     end
 
 end
 
-tree = OrangeTree.new
-tree.oneYearPasses
-tree.countTheOranges
+puts
+puts "WELCOME TO MY ORANGE TREE SIMULATOR".rjust(55)
+puts 
+puts '--> To start please type "plant" <--'.rjust(56)
+puts
+puts 'Type "help" for options'.rjust(48)
+
+while is_playing
+    user_input = gets.chomp.downcase
+
+    if user_input == "plant"
+        tree = OrangeTree.new
+    elsif user_input == "help"
+        puts "Command"
+    elsif user_input == "wait" && tree != 0
+        tree.oneYearPasses
+    elsif user_input == "age" && tree != 0
+        tree.age
+    elsif user_input == "pick" && tree != 0
+        tree.pickAnOrange
+    elsif user_input == "count" && tree != 0
+        tree.countTheOranges
+    elsif user_input == "height" && tree != 0
+        tree.height
+    elsif user_input == "date" && tree != 0
+        tree.date
+    elsif user_input == "exit" || user_input == "q"
+        is_playing = false
+    elsif user_input != "plant" || user_input != "wait" || user_input != "age" || user_input != "pick" || user_input != "count" || user_input != "height" || user_input != "date" || user_input != "exit"
+        puts "That is no command! Read the fucking guide! I can't hardcode every single command you are tinking of..."
+    else
+        puts "There is no tree planted, please plant one and move on"
+    end
+end
 
 
-#produserer ikke noe de første 3 årene
-#legge til countTheOranges(Hvor mange er på treet akkurat nå)
-#legge til pickAnOrange(Fjerner 1 fra treet og forteller hvor god den var, gjerne lag flere randomme svar her) eller at det ikke er flere igjen på treet i år
-#etter hvert år faller alle appelsinene av treet(countTheOranges)
 #legg alt inn i en loop slik at man kan bruke "gets" til å "spille" spillet
 
