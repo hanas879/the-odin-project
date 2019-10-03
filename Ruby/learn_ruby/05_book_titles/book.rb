1403f4d3ada445ca87ac684cd0e5bc3c37123c05
Henrik Enger Milo <hanas879@milo.life>
2019-10-03 15:21:30 +0200

    Start on last practice project

--- Ruby/learn_ruby/05_book_titles/book.rb
+++ Ruby/learn_ruby/05_book_titles/book.rb
@@ -1,4 +1,21 @@
 class Book
-    attr_reader :title
-    
+    def initialize
+        @title = "placeholder"
+    end
+
+    def title= title
+        @title = title
+    end
+
+    def title
+        conjunctions = ["and", "in", "the", "of", "a", "an"]
+        result = @title.split()
+        result.map do |word|
+            if !conjunctions.include?(word)
+                word.capitalize!
+            end
+        end
+        result.first.capitalize!
+        result.join(" ")
+    end
 end
--- Ruby/learn_ruby/06_timer/timer.rb
+++ Ruby/learn_ruby/06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds= seconds
+    @seconds = seconds
+  end
+  
+  def seconds
+    @seconds
+  end
 end
