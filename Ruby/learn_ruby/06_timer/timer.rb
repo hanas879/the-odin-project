e3c3d6ba1c142041648287d9fc45b3a1f8a66b19
Henrik Enger Milo <hanas879@milo.life>
2019-10-04 14:44:10 +0200

    Finish Learn Ruby exercises

--- Ruby/learn_ruby/06_timer/timer.rb
+++ Ruby/learn_ruby/06_timer/timer.rb
@@ -2,13 +2,45 @@ class Timer
   #write your code here
   def initialize
     @seconds = 0
-  end
-
-  def seconds= seconds
-    @seconds = seconds
+    @minutes = 0
+    @hours = 0
   end
   
   def seconds
     @seconds
+  end
+
+  def seconds= seconds
+    @seconds = seconds
+
+    if @seconds > 60
+      while @seconds > 60
+        @minutes += 1
+        @seconds -= 60
+      end
+    end
+
+    if @minutes > 60
+      while @minutes > 60
+        @hours += 1
+        @minutes -= 60
+      end
+    end
+  end
+  
+  def time_string
+    if @seconds < 10
+      @seconds = "0#{@seconds}"
+    end
+
+    if @minutes < 10
+      @minutes = "0#{@minutes}"
+    end
+
+    if @hours < 10
+      @hours = "0#{@hours}"
+    end
+
+    return "#{@hours}:#{@minutes}:#{@seconds}"
   end
 end
